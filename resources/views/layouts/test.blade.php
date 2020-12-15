df57c6afa4d26483f5c8ecdd91d015992aa05411
Jack Maloney <jackpmaloney2@gmail.com>
2020-12-15 15:54:58 +0000

    Updated CSS layout

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -2,26 +2,52 @@
 
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        @livewireStyles
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
         <title>Billboard - @yield('title')</title>
     </head>
-    <body>
-        <h1>Billboard - @yield('title')</h1>
-        <a href="{{ route('dashboard') }}">Login</a>
-        @if ($errors->any())
-            <div>
-                Errors: 
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        @if (session('message'))
-            <p><b>{{ session('message') }}</b></p>
-        @endif
-        <div class="container">
-            @yield('content')
+    <body class="font-sans antialiased">
+        
+        <div class="min-h-screen bg-gray-100">
+            @livewire('navigation-dropdown')
+
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <h1>Billboard - @yield('title')</h1>
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                @if ($errors->any())
+                <div>
+                    Errors: 
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            @if (session('message'))
+                <p><b>{{ session('message') }}</b></p>
+            @endif
+            <div class="container">
+                @yield('content')
+            </div> 
+            </main>
         </div>
+        @stack('modals')
+
+        @livewireScripts
+        
     </body>
 </html>
