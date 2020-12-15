c00280c963e0fb2a95e5b416e6df86bb07b5016a
Jack Maloney <jackpmaloney2@gmail.com>
2020-12-15 16:26:30 +0000

    Copied default CSS styling to views

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -16,7 +16,18 @@
     <body class="font-sans antialiased">
         
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-dropdown')
+            @if (Route::has('login'))
+                    @auth
+                        @livewire('navigation-dropdown')
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endif
+                </div>
+            @endif
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
