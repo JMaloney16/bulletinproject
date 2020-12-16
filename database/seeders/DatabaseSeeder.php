4c9dd7ca949bcd85d9ba44e3db27f7c92c1953e6
Jack Maloney <jackpmaloney2@gmail.com>
2020-12-16 17:18:31 +0000

    Commented code for admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,5 +21,15 @@ class DatabaseSeeder extends Seeder
         ->has(\App\Models\Post::factory()->count(3))
         ->create();
         \App\Models\Comment::factory(50)->create();
+        /*
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+            'is_admin' => '1',
+            'remember_token' => Str::random(10),
+        ]);
+        */
     }
 }
