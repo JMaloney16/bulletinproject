347a595fb6260a8f880cd2f7237133d9a46b9ec2
Jack Maloney <jackpmaloney2@gmail.com>
2020-12-29 19:31:52 +0000

    Published livewire views

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+Echo.private('commentNotification')
+  .listen('CommentPosted', (e) => {
+    this.messages.push({
+      message: e.message.comment,
+      user: e.user
+    });
+  });
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,11 +3,21 @@
 @section('title', 'User Details')
 
 @section('content')
-    <h1>{{ $user->name }}</h1>
-    <h2>Comments</h2>
-    <ul>
-        @foreach ($comments as $comment)
-            <li>Post ID:<a href="{{ route('posts.singlepost', ['post' => $comment->post_id]) }}">{{ $comment->post_id }}</a> - {{ $comment->content}}</li>
-        @endforeach
-    </ul>
+    <div class="px-4 md:px-12">
+        <div class="container bg-white rounded-lg shadow-lg my-6 mx-auto px-4 justify-center md:px-8">
+            <h1 class="text-xl font-bold my-6 pt-6">{{ $user->name }}</h1>
+            <h2 class="text-lg font-semibold my-4 pt-4">Comments</h2>
+            <ul>
+                @foreach ($comments as $comment)
+                    <li>Post ID:<a class="hover:underline"
+                            href="{{ route('posts.singlepost', ['post' => $comment->post_id]) }}">{{ $comment->post_id }}
+                            -
+                            {{ $comment->content }} </a>
+                    </li>
+                    <br>
+                @endforeach
+                {{ $comments->links() }}
+            </ul>
+        </div>
+    </div>
 @endsection
