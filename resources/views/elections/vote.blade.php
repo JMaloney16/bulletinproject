121031140a33854134d9ec844b0e461271667873
Jack Maloney <jackpmaloney2@gmail.com>
2021-04-02 15:45:58 +0100

    Added submit vote button

--- resources/views/elections/vote.blade.php
+++ resources/views/elections/vote.blade.php
@@ -9,17 +9,20 @@
 
 <div class="max-w-xl bg-white rounded-lg shadow-lg py-10 px-5 m-auto w-full mt-10">
     <div class="grid grid-cols-2 gap-4 max-w-xl m-auto">
-    <label>Candidates</label>
+    <label class="col-span-2">Candidates</label>
+    <form method="POST" action="#" enctype="multipart/form-data">
     @foreach ($candidates as $candidate)
         
-        <form method="POST" action="#" enctype="multipart/form-data">
             @csrf
-                <div class="col-span-2">
+                <div class="col-span-2 my-4">
                     <input type="radio" value={{ $candidate->id }} id={{ $candidate->id }} name="candidates">
                     <label for={{ $candidate->id }}>{{ $candidate->user->name}}</label>
                 </div>
+                @endforeach
+            <input type="submit" value="Submit Vote"
+                class="rounded-lg shadow-md px-2 py-2 border-black hover:bg-white hover:border-2">
+
             </form>
-            @endforeach
         </div>
 </div>
 
